98fd93eaa00890ba5c1440976299e1b368e35d99
何俊鹏 <1125440025@qq.com>
2020-02-14 21:18:14 +0800

    Second commit

--- Project1/1.c
+++ Project1/1.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main(void)
 {
-    printf("In the file %s, line %d, I set a printf in the function %s. Hope you like it!\n",__FILE__,__LINE__,__func__);
+    printf("Some var in C\n");
+    printf("In the file %s, line %d, I set a printf in the function %s.\n Hope you like it!\n",__FILE__,__LINE__,__func__);
     return 0;
 }
